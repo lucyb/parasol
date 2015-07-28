0f4e8afa58c76ba47d15adf55204374320e73ef7
Jon <jonathan.stott@gmail.com>
2015-07-28 20:46:08 +0100

    Update copyright

--- services/MobilePhotos.py
+++ services/MobilePhotos.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2015 Lucy B
+# Copyright 2015 Lucy B, Jonathan Stott
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
