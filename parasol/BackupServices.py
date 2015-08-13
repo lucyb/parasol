7d6404d5eeb4f42e8157cbb7aa9ab08d48357f36
lucyb <lucybridges@gmail.com>
2015-08-13 21:04:57 +0100

    Fix merge error

--- parasol/BackupServices.py
+++ parasol/BackupServices.py
@@ -48,7 +48,6 @@ class BackupServices(object):
                 service_name  = self.get_service_name(section_name)
                 service_class = self.service_registry().get(service_name)
                 BackupServices.run_backup(section_name, service_class, service_config)
-                self.run_backup(section_name, service_class, service_config)
             except ServiceNotFoundException:
                 click.echo('Found config section for {} but no matching service. Skipping'.format(section_name))
                 pass
