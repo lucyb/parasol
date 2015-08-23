f4f99d7756208a44a1830d1d204eb63701b03b61
lucyb <lucybridges@gmail.com>
2015-08-23 22:12:28 +0100

    Update filename creation after rebase from 34-correct-filenames

--- parasol/services/TinyTinyRss.py
+++ parasol/services/TinyTinyRss.py
@@ -30,7 +30,7 @@ class TinyTinyRss(AbstractService):
         self.verify_ssl = config.getboolean('verify_ssl', True)
 
     def do_backup(self):
-        filename = self.filename() + '.xml'
+        filename = self.filename(ext = 'xml')
         filepath = self.backup_path(filename)
 
         opml_file = self.connect()
