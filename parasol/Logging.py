6dba13b5c84344094a0c300083c764affcd82f0c
lucyb <lucybridges@gmail.com>
2015-09-04 20:49:34 +0100

    Add class that creates the Logger to the whitelist

--- parasol/Logging.py
+++ parasol/Logging.py
@@ -50,8 +50,9 @@ class LoggerWhitelist(logging.Filter):
     #Whitelist class from https://stackoverflow.com/a/17276457
     def __init__(self):
         #Fetch all parasol classes (from https://stackoverflow.com/a/8093671)
-        #This will include all classes imported into the namespace
+        #This will include all classes imported into the namespace and the class that creates the logger
         self.whitelist = [logging.Filter(clsmember[0]) for clsmember in inspect.getmembers(sys.modules[__name__], inspect.isclass)]
+        self.whitelist.append(logging.Filter("root"))
 
     def filter(self, record):
         return any(f.filter(record) for f in self.whitelist)
